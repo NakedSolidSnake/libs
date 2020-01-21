834598f376d99ccf55b2d1bd9a47e86110f4e7fc
Cristiano Silva <cristianosstec@gmail.com>
2020-01-21 09:09:25 -0300

    initial commit

---
+++ test_lib/test_time_meas.c
@@ -0,0 +1,26 @@
+#include <util/util.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  TimeMeas_t *t = createTimeMeas();
+  if(!t)
+    return EXIT_FAILURE;
+
+  getStartTime(t);
+  sleep(1);
+  getFinishTime(t);
+
+  long long resul = getDiffMeas(t);
+
+  if(resul < 1000)
+    return EXIT_FAILURE;
+
+  printf("time diff %ld\n", resul);
+
+  destroyTimeMeas(t);
+
+  return EXIT_SUCCESS;
+}
+
