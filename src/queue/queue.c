6f4a44b1bc3c365b13535453e60dd629849a0eed
Cristiano Silva <cristianosstec@gmail.com>
2020-07-12 10:37:32 -0300

    add timout queue recv

--- src/queue/queue.c
+++ src/queue/queue.c
@@ -4,14 +4,14 @@
 #include <unistd.h>
 #include <sys/msg.h>
 #include <queue/queue.h>
-#include <sys/time.h>
 #include <signal.h>
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
-void timer_handler(int signum)
-{
-    (void) signum;
-}
+
+static int compare(struct timespec *start, struct timespec *end, struct timespec *offset);
 
 int queue_init(int id)
 {
@@ -20,7 +20,7 @@ int queue_init(int id)
 
 int queue_send(int queue_id, const queue_st *data, const int bSize)
 {
-  if(bSize <= 0)
+  if (bSize <= 0)
     return -1;
 
   return msgsnd(queue_id, (void *)data, bSize, 0);
@@ -28,31 +28,43 @@ int queue_send(int queue_id, const queue_st *data, const int bSize)
 
 int queue_recv(int queue_id, queue_st *data, const int bSize, int timeout)
 {
-  struct sigaction sa;
-  struct itimerval timer;
+  struct timespec start, finish, offset; 
+  int flags = 0;
+  int ret = 0;
 
-  if(!data || bSize  <= 0)
+  if (!data || bSize <= 0)
     return -1;
 
-  if(timeout > 0)
+  if (timeout > 0)
   {
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = &timer_handler;
-    sigaction(SIGALRM, &sa, NULL);
-    
-    timer.it_value.tv_sec = timeout / 1000;
-    timer.it_value.tv_usec = (timeout % 1000) * 1000;
+ 
+    flags = IPC_NOWAIT;
 
-    timer.it_interval.tv_sec = timeout / 1000;
-    timer.it_interval.tv_usec = (timeout % 1000) * 1000;
+    offset.tv_sec = (timeout / 1000000000) ;
+    offset.tv_nsec = (timeout % 1000000000);
 
-    setitimer(ITIMER_REAL, &timer, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &start);
   }
 
-  return msgrcv(queue_id, (void *)data, bSize, data->queueType, 0);
+  do
+  {    
+    ret = msgrcv(queue_id, (void *)data, bSize, data->queueType, flags);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+  } while (compare(&start, &finish, &offset) && (ret < 0));
+
+  return ret;
 }
 
 int queue_destroy(int queue_id)
 {
   return msgctl(queue_id, IPC_RMID, 0);
+}
+
+static int compare(struct timespec *start, struct timespec *end, struct timespec *offset)
+{
+  double _start = (double)start->tv_sec + (double)start->tv_nsec / (double)1000000000;
+  double _end = (double)end->tv_sec + (double)end->tv_nsec / (double)1000000000;
+  double _offset = (double)offset->tv_sec + (double)offset->tv_nsec / (double)1000000000;
+
+  return ((_start + _offset) > _end);
 }
