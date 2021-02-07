6acebd11a2138c64bd5502ff148654ab3a91ed9b
Cristiano Silva <cristianosstec@gmail.com>
2021-02-07 12:11:54 -0300

    add new functions to FILE library

--- include/file/file.h
+++ include/file/file.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+int FILE_getSize(FILE *file);
+int FILE_getFileContent(const char *filename, const char *mode, char **buffer);
+int FILE_write(const char *filename, const char *mode, const char *buffer);
+
 FILE *openWrite(const char *filename);
 FILE *openRead(const char *filename);
 size_t getFileSize(FILE *file);
--- src/file/file.c
+++ src/file/file.c
@@ -1,6 +1,36 @@
 #include <stdlib.h>
 #include <file/file.h>
 
+int FILE_getSize(FILE *file)
+{
+  size_t len = 0;
+  if(!file)
+    return -1;
+  fseek(file, 0, SEEK_END);
+  len = ftell(file);
+  rewind(file);
+  return len;
+}
+
+int FILE_getFileContent(const char *filename, const char *mode, char **buffer)
+{
+  FILE *file = fopen(filename, mode);
+  if(file == NULL)
+    return -1;
+
+  size_t size = FILE_getSize(file);
+
+  *buffer = (char *)malloc(sizeof(char) * size + 1);
+  fread(*buffer, 1, size, file);
+  fclose(file);
+  return 0;
+}
+
+int FILE_write(const char *filename, const char *mode, const char *buffer)
+{
+  return -1;
+}
+
 FILE *openWrite(const char *filename)
 {
   FILE *fd = fopen(filename, "w");
