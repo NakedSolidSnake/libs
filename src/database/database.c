2e587795343394d970554d42ee04c8daa69db7e4
Cristiano Silva <cristianosstec@gmail.com>
2021-02-09 19:57:21 -0300

    refactor database

--- src/database/database.c
+++ src/database/database.c
@@ -6,31 +6,52 @@ static MYSQL connection;
 
 int Database_init(const char *hostname, const char *username, const char *password, const char *database)
 {
-  MYSQL *ret = NULL;
-  mysql_init(&connection);
+    MYSQL *ret = NULL;
+    mysql_init(&connection);
 
-  ret = mysql_real_connect(&connection, hostname, username, password, database, 0, NULL, 0);  
-  return ret == NULL ? EXIT_FAILURE : EXIT_SUCCESS;
+    ret = mysql_real_connect(&connection, hostname, username, password, database, 0, NULL, 0);
+    return ret == NULL ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 int Database_queryExec(char *(*query)(void *data), void *data)
 {
-  int ret = -1; 
-  if(!query){
-    return -1;
-  }
+    int ret = -1;
+    if (!query)
+    {
+        return -1;
+    }
 
-  if(!data){
-    return -1;
-  }
+    if (!data)
+    {
+        return -1;
+    }
 
-  ret = mysql_query(&connection, query(data));
-  
-  return !ret ? EXIT_FAILURE : EXIT_SUCCESS;
+    ret = mysql_query(&connection, query(data));
+
+    return !ret ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+int Database_resultSet(void (*Database_get)(char **data, int rows, void *user_data), void *user_data)
+{
+    MYSQL_RES *result = mysql_store_result(&connection);
+    if (result == NULL)
+        return -1;
+
+    int num_fields = mysql_num_fields(result);
+    int num_rows = mysql_affected_rows(&connection);
+
+    MYSQL_ROW row;
+
+    while ((row = mysql_fetch_row(result)))
+    {
+        Database_get(row, num_rows, user_data);
+    }
+
+    mysql_free_result(result);
 }
 
 int Database_close(void)
 {
-  mysql_close(&connection);
-  return 0;
+    mysql_close(&connection);
+    return 0;
 }
